ceb9448ea981909eef3971eacf6ff1ec6bf409a8
EyifaDavid <102861958+EyifaDavid@users.noreply.github.com>
2025-10-21 20:30:35 +0000

    production

--- backend/utils/index.js
+++ backend/utils/index.js
@@ -18,10 +18,10 @@ export const createJWT = (res, userId)=> {
 
     res.cookie("token", token,{
         httpOnly: true,
-        // secure: process.env.NODE_ENV !== "production",
+        secure: process.env.NODE_ENV !== "production",
         secure:true,
         // sameSite:  process.env.NODE_ENV === "production" ? "none" : "lax", //prevent CSRF attack
-        sameSite:"None",
+        sameSite:"Strict",
         maxAge: 1 * 24 * 60 * 60 * 1000, //a day
     })
 };
