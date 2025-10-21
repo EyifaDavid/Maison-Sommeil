2b89662d88043a7cb5fcc991faa15b364fa3a260
EyifaDavid <102861958+EyifaDavid@users.noreply.github.com>
2025-10-21 18:32:50 +0000

    Update

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,6 @@ import userRoutes from "./userRoutes.js";
 import uploadRoutes from "./uploadRoutes.js";
 import authRoutes from "./authRoutes.js";
 import cartRoutes from "./cartRoutes.js";
-import tryonRoutes from "./tryonRoutes.js";
 import orderRoutes from "./orderRoutes.js";
 import adminRoutes from "./adminRoutes.js";
 
@@ -15,7 +14,6 @@ router.use('/users', userRoutes);
 router.use('/upload', uploadRoutes);
 router.use('/cart', cartRoutes);
 router.use('/auth', authRoutes);
-router.use('/tryon', tryonRoutes);
 router.use('/orders', orderRoutes);
 router.use('/admin', adminRoutes);
 
